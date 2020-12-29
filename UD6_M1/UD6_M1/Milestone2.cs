a1265e36ed9fb51772b5f65ddc2bad9656451412
Steelnox <eric14pastor@gmail.com>
2020-12-29 10:10:07 +0100

    Milestone 2 resuelta y arreglo metodo main (switch)

---
+++ UD6_M1/UD6_M1/Milestone2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD6_M1
+{
+    public class Milestone2
+    {
+        public void Principal()
+        {
+            double double_var = 4.5678;
+            int int_var = (int)double_var;
+            float float_var = (float)double_var;
+            string string_var = double_var.ToString();
+
+            Console.WriteLine("La variable double es " + double_var);
+            Console.WriteLine("La variable int es " + int_var);
+            Console.WriteLine("La variable float es " + float_var);
+            Console.WriteLine("La variable string es " + string_var);
+        }
+    }
+}
--- UD6_M1/UD6_M1/Program.cs
+++ UD6_M1/UD6_M1/Program.cs
@@ -7,8 +7,24 @@ namespace UD6_M1
         static void Main(string[] args)
         {
             Milestone1 m1 = new Milestone1();
+            Milestone2 m2 = new Milestone2();
 
-            m1.Principal();
+            string opcion = "";
+
+            Console.WriteLine("Seleccione una milestone del 1 al 3");
+            opcion = Console.ReadLine();
+            switch (opcion)
+            {
+                case "1":
+                    m1.Principal();
+                    break;
+                case "2":
+                    m2.Principal();
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
